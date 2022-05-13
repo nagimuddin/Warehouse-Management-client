c47302cd938e5d797296a114de7a596426ba3f29
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-05-13 22:37:48 +0600

    display section complited

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Bennar from '../Shared/Bennar/Bennar';
 import Items from '../Items/Items';
+import Brand from '../Shared/Brand/Brand';
+import Display from '../Shared/Display/Display';
 
 const Home = () => {
     return (
         <div>
             <Bennar></Bennar>
             <Items></Items>
+            <Display></Display>
+            <Brand></Brand>
         </div>
     );
 };
